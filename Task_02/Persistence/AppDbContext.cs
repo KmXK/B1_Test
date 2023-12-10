be6f706e5fee4d8768b983400a70dc0e5a1055e0
KmX <guydo.k@yandex.by>
2023-12-10 19:43:13 +0300

    Created DbContext

---
+++ Task_02/Persistence/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task_02.Persistence;
+
+public class AppDbContext(string connectionString) : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
---
+++ Task_02/Persistence/ApplicationDbContextDesignFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Task_02.Persistence;
+
+public class AppDbContextDesignFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<AppDbContext>();
+        builder.UseSqlServer();
+
+        return new AppDbContext(null!);
+    }
+}
